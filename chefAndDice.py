971ffc2b15fdc815f05d7bac39c45c3c057bb3b4
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-04-17 08:08:09 +0530

    Add files via upload

---
+++ chefAndDice.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 08:05:57 2021
+
+@author: raj patil
+"""
+
+# cook your dish here
+test = int(input())
+for tests in range(test):
+    n = int(input())
+    if n == 1:
+        print(20)
+    elif n == 2:
+        print(36)
+    elif n == 3:
+        print(51)
+    elif n == 4:
+        print(60)
+    else:
+        rem = n%4
+        ans = ((n-rem)//4)*44
+        if rem == 0:
+            ans += 16
+        elif rem == 1:
+            ans += 32
+        elif rem == 2:
+            ans += 44
+        elif rem == 3:
+            ans += 55
+        print(ans)
+        
+    
